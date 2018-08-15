24c3483d3f4c8194f15e13b3f3ccbe1331e8d087
Rafael Ribeiro de Sousa <faelribeirodesousa@gmail.com>
2018-08-15 15:42:46 -0300

    Create second enemy

---
+++ Assets/Scripts/Crab.cs
@@ -0,0 +1,92 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Crab : MonoBehaviour {
+
+    public int health;
+    public GameObject particleEffect;
+    int direction;
+    float timer = 1.5f;
+    SpriteRenderer spriteRenderer;
+    public float speed;
+    public Sprite facingUp;
+    public Sprite facingDown;
+    public Sprite facingLeft;
+    public Sprite facingRight;
+
+    // Use this for initialization
+    void Start () {
+        spriteRenderer = GetComponent<SpriteRenderer>();
+        spriteRenderer.sprite = facingUp;
+        direction = Random.Range(0, 3);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        timer -= Time.deltaTime;
+        if(timer <= 0)
+        {
+            timer = 1.5f;
+            direction = Random.Range(0, 3);
+        }
+        Movement();
+
+    }
+
+    void Movement()
+    {
+        switch(direction)
+        {
+            case 0:
+                transform.Translate(0, -speed * Time.deltaTime, 0);
+                spriteRenderer.sprite = facingDown;
+                break;
+            case 1:
+                transform.Translate(-speed * Time.deltaTime, 0, 0);
+                spriteRenderer.sprite = facingLeft;
+                break;
+            case 2:
+                transform.Translate(speed * Time.deltaTime, 0, 0);
+                spriteRenderer.sprite = facingRight;
+                break;
+            case 3:
+                transform.Translate(0, speed * Time.deltaTime, 0);
+                spriteRenderer.sprite = facingUp;
+                break;
+        }
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.gameObject.tag == "Player")
+        {
+            health--;
+            collision.gameObject.GetComponent<Player>().currentHealth--;
+
+            if (health <= 0)
+            {
+                Instantiate(particleEffect, transform.position, transform.rotation);
+                Destroy(gameObject);
+            }
+        }
+
+        if (collision.tag == "Sword")
+        {
+            health--;
+            if(health<=0)
+            {
+                Instantiate(particleEffect, transform.position, transform.rotation);
+                Destroy(gameObject);
+            }
+            collision.GetComponent<Sword>().CreateParticle();
+            GameObject.FindGameObjectWithTag("Player").GetComponent<Player>().canAttack = true;
+            Destroy(collision.gameObject);
+        }
+
+        if (collision.gameObject.tag == "Wall")
+        {
+            direction = Random.Range(0, 3);
+        }
+    }
+}
--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -9,14 +9,19 @@ public class Player : MonoBehaviour {
     Animator anim;
     public Image[] hearts;
     public int maxHealth;
-    int currentHealth;
+    public int currentHealth;
     public GameObject sword;
+    public float thrustPower;
+    public bool canMove;
+    public bool canAttack;
 
 	// Use this for initialization
 	void Start () {
         anim = GetComponent<Animator>();
         currentHealth = maxHealth;
         getHealth();
+        canMove = true;
+        canAttack = true;
 
     }
 	
@@ -50,6 +55,10 @@ public class Player : MonoBehaviour {
         /*
          * forma que era feita: Input.GetKey(KeyCode.S)
          */
+         if(!canMove)
+        {
+            return;
+        }
         if (Input.GetAxisRaw("Vertical") > 0)
         {
             anim.SetInteger("dir", 0);
@@ -82,24 +91,43 @@ public class Player : MonoBehaviour {
 
     void Attack()
     {
+        if (!canAttack)
+        {
+            return;
+        }
+        canMove = false;
+        canAttack = false;
         GameObject newSword = Instantiate(sword, transform.position, sword.transform.rotation);
+        if(currentHealth == maxHealth )
+        {
+            newSword.GetComponent<Sword>().special = true;
+            canMove = true;
+            thrustPower = 500;
+        }
+        #region //SwordRotation
         int swordDir = anim.GetInteger("dir");
+        anim.SetInteger("attackDir", swordDir);
         if (swordDir == 0)
         {
             newSword.transform.Rotate(0, 0, 0);
+            newSword.GetComponent<Rigidbody2D>().AddForce(Vector2.up * thrustPower);
         }
         else if (swordDir == 1)
         {
             newSword.transform.Rotate(0, 0, 180);
+            newSword.GetComponent<Rigidbody2D>().AddForce(Vector2.up * -thrustPower);
         }
         else if (swordDir == 2)
         {
             newSword.transform.Rotate(0, 0, 90);
+            newSword.GetComponent<Rigidbody2D>().AddForce(Vector2.right * -thrustPower);
         }
         else if (swordDir == 3)
         {
             newSword.transform.Rotate(0, 0, -90);
+            newSword.GetComponent<Rigidbody2D>().AddForce(Vector2.right * thrustPower);
         }
+        #endregion
     }
 
     void startSpeedAnim()
